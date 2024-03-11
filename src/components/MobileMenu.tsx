defd0ebe9fc15c02918a5956e9c8e3a33746cb50
hp0098v1 <hp0098v1@gmail.com>
2024-03-11 16:45:25 +0330

    fix mobile nav issue

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react"
-import { AiOutlineMenu } from "react-icons/ai"
-import { Link, useNavigate } from "react-router-dom"
-import { useTranslation } from "react-i18next"
+import { useEffect, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 // import ChangeLangButton from "./ChangeLangButton"
-import { menuItems } from "../utils/http"
-import { menuItemOptions } from "../utils/http/menuItems"
-import { HashLink } from 'react-router-hash-link';
+import { menuItems } from "../utils/http";
+import { menuItemOptions } from "../utils/http/menuItems";
+import { HashLink } from "react-router-hash-link";
 // import {
 //     Accordion,
 //     AccordionButton,
@@ -14,62 +14,61 @@ import { HashLink } from 'react-router-hash-link';
 //     AccordionPanel,
 //     Box
 // } from "@chakra-ui/react"
-import { ApplicationRoutes } from "../routes"
-import getBaseUrl from "../utils/base-url"
+import { ApplicationRoutes } from "../routes";
+import getBaseUrl from "../utils/base-url";
+import LoginStateButton from "./shared/LoginStateButton";
 
 const getLang = (lng) => {
-    const languages = ["en", "fa"]
-    return languages.find(v => v === lng) || "fa"
-}
+  const languages = ["en", "fa"];
+  return languages.find((v) => v === lng) || "fa";
+};
 
 function MobileMenu() {
-    const [isOpen, setIsOpen] = useState(false)
-    const [t, i18n] = useTranslation()
-    const [items, setItems] = useState<menuItemOptions>()
-    // const navigate = useNavigate()
-    
+  const [isOpen, setIsOpen] = useState(false);
+  const [t, i18n] = useTranslation();
+  const [items, setItems] = useState<menuItemOptions>();
+  // const navigate = useNavigate()
 
-    useEffect(
-        () => {
-            menuItems({ lng: getLang(i18n.language) })
-                .then(data => setItems(data))
-        },
-        [i18n.language]
-    )
+  useEffect(() => {
+    menuItems({ lng: getLang(i18n.language) }).then((data) => setItems(data));
+  }, [i18n.language]);
 
-
-    return (
-        <>
-            <button onClick={() => setIsOpen(true)} className="p-2 rounded-lg bg-white/10 grid place-items-center active:scale-95 transition-transform duration-200">
-                <AiOutlineMenu className="w-5 h-5 fill-slate-100" />
-            </button>
-            <div
-                className={`fixed top-0 left-0 w-full h-full pointer-events-none z-50
+  return (
+    <>
+      <button
+        onClick={() => setIsOpen(true)}
+        className="p-2 rounded-lg bg-white/10 grid place-items-center active:scale-95 transition-transform duration-200"
+      >
+        <AiOutlineMenu className="w-5 h-5 fill-slate-100" />
+      </button>
+      <div
+        className={`fixed top-0 left-0 w-full h-full pointer-events-none z-50
                 ${isOpen ? "!pointer-events-auto" : ""}
                 `}
-            >
-                <div
-                    id="mobile-menu-back"
-                    onClick={() => setIsOpen(false)}
-                    className={`absolute top-0 left-0 z-10 w-full h-full bg-black/40 pointer-events-none opacity-0
+      >
+        <div
+          id="mobile-menu-back"
+          onClick={() => setIsOpen(false)}
+          className={`absolute top-0 left-0 z-10 w-full h-full bg-black/40 pointer-events-none opacity-0
                     transition-opacity duration-300
                     ${isOpen ? "!opacity-100 !pointer-events-auto" : ""}`}
-                ></div>
+        ></div>
 
-                <div
-                    className={`w-64 h-full bg-gray-900 absolute top-0 right-0 translate-x-full z-20
+        <div
+          className={`w-64 h-full bg-gray-900 absolute top-0 right-0 translate-x-full z-20
                     gap-y-3 pt-6 grid grid-rows-[max-content_1fr_max-content]
-                    transition-transform duration-300 ${isOpen ? "!translate-x-0" : ""}`}
-                >
+                    transition-transform duration-300 ${
+                      isOpen ? "!translate-x-0" : ""
+                    }`}
+        >
+          <Link
+            to="/ecommerce-webapp_vite_react"
+            className="text-2xl font-[iranyekan500] text-slate-50 block mx-auto"
+          >
+            {t("mobile-menu.title")}
+          </Link>
 
-                    <Link
-                        to="/ecommerce-webapp_vite_react"
-                        className="text-2xl font-[iranyekan500] text-slate-50 block mx-auto"
-                    >
-                        {t("mobile-menu.title")}
-                    </Link>
-
-                    {/* <div dir={i18n.dir(i18n.language)} className="flex flex-col gap-y-2 px-3 mt-5">
+          {/* <div dir={i18n.dir(i18n.language)} className="flex flex-col gap-y-2 px-3 mt-5">
                         {
                             items?.items?.map(item => (
                                 <Link
@@ -84,50 +83,62 @@ function MobileMenu() {
                         }
                     </div> */}
 
-                    <div onClick={() => document.getElementById("mobile-menu-back").click()} className="flex flex-col gap-y-3 px-2">
-
-                        <Link
-                            to={ApplicationRoutes.pages.home}
-                            className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
+          <div
+            onClick={() => document.getElementById("mobile-menu-back").click()}
+            className="flex flex-col gap-y-3 px-2"
+          >
+            <Link
+              to={ApplicationRoutes.pages.home}
+              className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
                             hover:bg-white/5 rounded-lg transition-colors duration-300"
-                        >
-                            {t("header.home")}
-                        </Link>
+            >
+              {t("header.home")}
+            </Link>
 
-                        <HashLink
-                            to={getBaseUrl()+"#about-us-section"}
-                            className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
+            <HashLink
+              to={getBaseUrl() + "#about-us-section"}
+              className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
                             hover:bg-white/5 rounded-lg transition-colors duration-300"
-                        >
-                            {t("about-us.title")}
-                        </HashLink>
+            >
+              {t("about-us.title")}
+            </HashLink>
 
-                        <HashLink
-                            to={getBaseUrl()+"#our-services-section"}
-                            className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
+            {/* <HashLink
+              to={getBaseUrl() + "#our-services-section"}
+              className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
                             hover:bg-white/5 rounded-lg transition-colors duration-300"
-                        >
-                            {t("home.services.title")}
-                        </HashLink>
+            >
+              {t("home.services.title")}
+            </HashLink> */}
 
-                        <HashLink
-                            to={getBaseUrl()+"#our-projects-section"}
-                            className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
+            <Link
+              to={ApplicationRoutes.pages.work.project}
+              className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
                             hover:bg-white/5 rounded-lg transition-colors duration-300"
-                        >
-                            {t("home.projects.title")}
-                        </HashLink>
+            >
+              Projects
+            </Link>
 
-                        <Link
-                            to={ApplicationRoutes.pages.contact}
-                            className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
+            <Link
+              to={ApplicationRoutes.pages.blog.blog}
+              className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
                             hover:bg-white/5 rounded-lg transition-colors duration-300"
-                        >
-                            {t("header.contact")}
-                        </Link>
-                    </div>
+            >
+              Blogs
+            </Link>
 
-                    {/* <Accordion
+            <Link
+              to={ApplicationRoutes.pages.contact}
+              className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide
+                            hover:bg-white/5 rounded-lg transition-colors duration-300"
+            >
+              {t("header.contact")}
+            </Link>
+
+            <LoginStateButton className="p-2.5 text-white text-sm font-[iranyekan400] tracking-wide text-left hover:bg-white/5 rounded-lg transition-colors duration-300" />
+          </div>
+
+          {/* <Accordion
                         allowMultiple
                         display="flex"
                         flexDirection="column"
@@ -189,17 +200,14 @@ function MobileMenu() {
 
                     </Accordion> */}
 
-
-                    {/* <div className="mt-auto flex items-center gap-x-2 justify-center mb-6 mx-auto w-max rounded-xl
+          {/* <div className="mt-auto flex items-center gap-x-2 justify-center mb-6 mx-auto w-max rounded-xl
                     border border-slate-700">
                         <ChangeLangButton />
                     </div> */}
-
-
-                </div>
-            </div>
-        </>
-    )
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MobileMenu
+export default MobileMenu;
--- src/components/shared/LoginStateButton.tsx
+++ src/components/shared/LoginStateButton.tsx
@@ -2,25 +2,24 @@ import React from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-const LoginStateButton = () => {
+const initialClasses =
+  "text-white text-lg font-medium tracking-wide py-3 px-1.5 relative hover:text-gray-400 group transition-colors duration-300 inline-block";
+
+interface Props {
+  className?: string;
+}
+
+const LoginStateButton = ({ className = initialClasses }: Props) => {
   const { user, logout } = useAuth();
 
   const navigate = useNavigate();
 
   return user !== null ? (
-    <button
-      className="text-white text-lg font-medium tracking-wide py-3 px-1.5 relative hover:text-gray-400
-  group transition-colors duration-300 inline-block"
-      onClick={logout}
-    >
+    <button className={className} onClick={logout}>
       Logout
     </button>
   ) : (
-    <button
-      className="text-white text-lg font-medium tracking-wide py-3 px-1.5 relative hover:text-gray-400
-  group transition-colors duration-300 inline-block"
-      onClick={() => navigate("/sign-in")}
-    >
+    <button className={className} onClick={() => navigate("/sign-in")}>
       Sign In
     </button>
   );
